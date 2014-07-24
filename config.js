88e219698eee0a5124b3bcf973f7417eb0f9916a
Eric Weyant <weyanteric@gmail.com>
2014-07-24 14:15:52 -0400

    updated config

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
 		db: 'mongodb://weyanteric:Zxcvbnm#1@ds053449.mongolab.com:53449/smarter-health', // MongoDB connection string, ex: mongodb://db-user:db-password@mongo.onmodulus.net:27017/1234567
 		fitbitClientKey: '61b393fcee444af389dc08333aa66d1c', // Your Fitbit application information found at https://dev.fitbit.com/apps
 		fitbitClientSecret: '545a8367dd894e8cb884a6621ff62128',
-		host: 'health-smart.herokuapp.com', // The hostname where this application is available publicly, ex: fitbitexample-9501.onmodulus.net
+		host: 'smarter-health.herokuapp.com', // The hostname where this application is available publicly, ex: fitbitexample-9501.onmodulus.net
 	}
 };
